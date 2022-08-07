71b10ce9dc4dd1dcf888fb239b3a4b7c83ecfd42
kamilksi <kamilksiazek002@gmail.com>
2022-08-07 06:49:49 +0200

    Initial Commit

---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Kamil Książek on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
