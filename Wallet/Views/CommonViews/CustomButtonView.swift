6e1b06f0ea912df0ba0e9d9f8b7d7b1374b8fcbb
kamilksi <kamilksiazek002@gmail.com>
2022-08-25 22:45:32 +0200

    Custom button

---
+++ Wallet/Views/CommonViews/CustomButtonView.swift
@@ -0,0 +1,29 @@
+//
+//  CustomButtonView.swift
+//  Wallet
+//
+//  Created by Kamil Książek on 25/08/2022.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var buttonName = ""
+    var body: some View {
+        Button(action: {}, label: {
+            Text(buttonName)
+                .fontWeight(.semibold)
+                .foregroundColor(.black)
+                .frame(width: 350, height: 50)
+                .background(Color(.init(white: 1, alpha: 0.15)))
+                .clipShape(Capsule())
+
+        })
+    }
+}
+
+struct CustomButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButtonView(buttonName: "Sign Up")
+    }
+}
